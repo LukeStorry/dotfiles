12f86ebe15a59225f1f62b9f204d65a82e3d6c97
Luke Storry <luke@lukestorry.co.uk>
2019-11-06 16:29:20 +0000

    added fancy gitprompt

---
+++ git-prompt.sh
@@ -0,0 +1,23 @@
+PS1='\[\033]0;${PWD#*code/}\007\]' # set window title, minus code dir
+PS1="$PS1"'\n'                 # new line
+PS1="$PS1"'\[\033[36m\]'       # change to cyan
+PS1="$PS1"'\u@\h '             # user@host<space>
+PS1="$PS1"'\[\033[34m\]'       # change to blue
+PS1="$PS1"'\w'                 # current working directory
+if test -z "$WINELOADERNOEXEC"
+then
+    GIT_EXEC_PATH="$(git --exec-path 2>/dev/null)"
+    COMPLETION_PATH="${GIT_EXEC_PATH%/libexec/git-core}"
+    COMPLETION_PATH="${COMPLETION_PATH%/lib/git-core}"
+    COMPLETION_PATH="$COMPLETION_PATH/share/git/completion"
+    if test -f "$COMPLETION_PATH/git-prompt.sh"
+    then
+        . "$COMPLETION_PATH/git-completion.bash"
+        . "$COMPLETION_PATH/git-prompt.sh"
+        PS1="$PS1"'\[\033[92m\]'  # change color to light green
+        PS1="$PS1"'`__git_ps1`'   # bash function
+    fi
+fi
+PS1="$PS1"'\[\033[0m\]'        # change color to default
+PS1="$PS1"'\n'                 # new line
+PS1="$PS1"'$ '                 # prompt: always $
